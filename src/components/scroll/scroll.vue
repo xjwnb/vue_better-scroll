b8e1ea9d9ea8e5e706c5e84147c1ede7d2ef3070
xjwnb <1565163445@qq.com>
2020-06-27 22:24:10 +0800

    修改部分

---
+++ src/components/scroll/scroll.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  name: 'scroll',
+  props: {
+    probeType: {
+      type: Number,
+      default() {
+        return 0
+      }
+    }
+  },
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType
+    })
+
+    if (this.probeType === 2 || this.probeType === 3) {
+      this.scroll.on('scroll', position => {
+        console.log(position)
+        // console.log('---')
+      })
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
